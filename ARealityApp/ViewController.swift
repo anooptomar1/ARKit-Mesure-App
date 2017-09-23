06fcac66cc6685ff8c393079f370fc68312b57f8
Unknown <unbe4table@protonmail.com>
2017-09-23 20:22:09 +0300

    commit

--- ARealityApp/ViewController.swift
+++ ARealityApp/ViewController.swift
@@ -21,9 +21,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
         // Create a new scene
+        setupCamera()
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
@@ -38,6 +39,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    func createBall(position: SCNVector3) {
+        let ballShape = SCNSphere(radius: 0.01)
+        let ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = position
+        sceneView.scene.rootNode.addChildNode(ballNode)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let result = sceneView.hitTest(touch.location(in: sceneView),types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitResult = result.last else { return }
+        let hitTransform = SCNMatrix4((hitResult.worldTransform))
+        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        createBall(position: hitVector)
+    }
+    
+    func setupCamera() {
+        guard let camera = sceneView.pointOfView?.camera else {
+            fatalError("Expected a valid `pointOfView` from the scene.")
+        }
+        
+        camera.wantsHDR = true
+        camera.exposureOffset = -1
+        camera.minimumExposure = -1
+        camera.maximumExposure = 3
     }
     
     override func viewWillDisappear(_ animated: Bool) {
